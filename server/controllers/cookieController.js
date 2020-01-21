d3679fc8d088fe645aa4aef46b30075ebbce0c77
EggsBenedict2123 <EggsBenedict2123@users.noreply.github.com>
2020-01-21 11:11:36 -0500

    all the marbles

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,10 +1,17 @@
+const cookieParser = require("cookie-parser");
 const cookieController = {};
 
 cookieController.createCookie = (req, res, next) => {
   // Create cookie based on user ID saved in res.locals
+  if (!req.cookies.userId) res.cookie("userId", res.locals.userId);
+  next();
 };
 
 cookieController.checkCookie = (req, res, next) => {
   // check if the user attempting to join page has cookie.
   // If they do have a cookie, send them to the landing page
+  if (!req.cookies.userId) res.redirect("/login");
+  else next();
 };
+
+module.exports = cookieController;
