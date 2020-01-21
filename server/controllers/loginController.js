f9643713d04bb082f33301cbcb8ed2e5ba81674a
EggsBenedict2123 <EggsBenedict2123@users.noreply.github.com>
2020-01-21 12:18:05 -0500

    all the marbles

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,3 +1,4 @@
+const pool = require("../models/userModel.js");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 // Handles logging in and grabbing the data from our database. If no account is found, add user to DB and send them to a pokemon grabbing controller
@@ -11,10 +12,36 @@ loginController.verifyUser = (req, res, next) => {
 };
 
 // Controller for handling creating a user
-loginController.createUser = (req, res, next) => {
+loginController.createUser = async (req, res, next) => {
   const { user, pass } = req.body;
+  // First check DB to see if username has been used before
+  // Optimize rows: data
+  const { rows: data } = await pool.query(
+    `SELECT username FROM users WHERE username = '${user}'`
+  );
+  if (data.length) return res.render("Username Already Exists");
+
   bcrypt.hash(pass, saltRounds, (err, hash) => {
+    if (err) return console.log(err);
     // Store hash to SQL db
+    pool.query(
+      `INSERT INTO users(user_id, username, password) VALUES(default, '${user}', '${hash}')`,
+      error => {
+        if (error) return console.error("error running query", error);
+        pool.end();
+      }
+    );
   });
   next();
 };
+
+// async function testDrive(user, pass) {
+//   let userExists = true;
+//   const { rows: data } = await pool.query(
+//     `SELECT username FROM users WHERE username = '${user}'`
+//   );
+//   console.log(data);
+//   return;
+// }
+
+// testDrive("rexosariemen", "1232343sfa");
